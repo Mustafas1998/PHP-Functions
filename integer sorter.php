a885fbf620692fd6a5cb223f508d7ad40ab50f9d
Mustafas1998 <ms6534126@gmail.com>
2024-03-11 22:58:01 +0200

    Integer sorter function

---
+++ integer sorter.php
@@ -0,0 +1,17 @@
+<?php
+function filter($text)
+{
+    $myarray = array();
+    foreach (str_split($text) as $char)
+    {
+     if (ctype_digit($char))  
+        {
+            array_push($myarray,$char);
+            
+        }
+    }asort($myarray);
+
+    print_r($myarray);
+}
+
+filter("123jdfsdjf81293!@&!$*!@");
